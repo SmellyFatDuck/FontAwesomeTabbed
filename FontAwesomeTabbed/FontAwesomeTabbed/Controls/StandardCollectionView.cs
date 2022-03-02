af51c99ae7740f0873dcbf636225bc0b41d68523
Micael Roche <micaeljoshuaroche@hotmail.com>
2022-03-02 12:08:54 +1100

    Updated features

--- FontAwesomeTabbed/FontAwesomeTabbed/Controls/StandardCollectionView.cs
+++ FontAwesomeTabbed/FontAwesomeTabbed/Controls/StandardCollectionView.cs
@@ -1,5 +1,4 @@
 ﻿using FontAwesomeTabbed.Models;
-using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace FontAwesomeTabbed.Controls
@@ -17,40 +16,13 @@ namespace FontAwesomeTabbed.Controls
 
         private static void ScrollIndexChanged(BindableObject bindable, object oldValue, object newValue)
         {
-            Debug.WriteLine("Property Changed!");
-            if (newValue == null)
-                return;
-
-            Debug.WriteLine("Has new value!");
-            if (bindable is StandardCollectionView current)
-                if (newValue is Icon scrollToItem)
-                    current.ScrollTo(scrollToItem);
-
-            Debug.WriteLine("Should be scrolling to item...");
-        }
-        /*public static BindableProperty ScrollToItemProperty =
-            BindableProperty.Create(nameof(ScrollToItem), 
-                                    typeof(Icon), 
-                                    typeof(StandardCollectionView),
-                                    typeof(Icon),
-                                    BindingMode.Default,
-                                    propertyChanged: ScrollIndexChanged);
-
-        public Icon ScrollToItem
-        {
-            get => (Icon)GetValue(ScrollToItemProperty);
-            set => SetValue(ScrollToItemProperty, value);
-        }
-
-        private static void ScrollIndexChanged(BindableObject bindable, object oldValue, object newValue)
-        {
             if (newValue == null)
                 return;
 
             if (bindable is StandardCollectionView current)
                 if (newValue is Icon scrollToItem)
                     current.ScrollTo(scrollToItem);
-        }*/
+        }
 
     }
 }
